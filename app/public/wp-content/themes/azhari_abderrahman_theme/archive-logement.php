9e903fad0b6031375e74479a264401af4ca2137c
Abdou <abdouzozo.14@gmail.com>
2020-06-22 16:26:17 +0200

    Ajout style logement

--- app/public/wp-content/themes/azhari_abderrahman_theme/archive-logement.php
+++ app/public/wp-content/themes/azhari_abderrahman_theme/archive-logement.php
@@ -2,16 +2,36 @@
 <h1 > Liste des logements </h1>
 <?php $monPost = new WP_Query( array( 'post_type' => 'logement', 'posts_per_page' => '6' ) ); ?>
 <?php if (have_posts()) : ?>
-    <div >
+    <div class="row">
         <?php while ($monPost->have_posts()) : $monPost->the_post(); ?>
+
+            
+            <div class="column">
+            <div class="center">
+                <div class="property-card-nohover">
+                    <a href="#">
+                        <div class="property-image" >
+                            
+                                <?php the_post_thumbnail('medium') ?>
+                            
+                        </div>
+                    </a>
+                    <div class="property-description">
+                        <h5> <?php the_field("nom") ?> </h5>
+                        <p>Un bien de type : <?php the_field("type")?>. 
+                            Prix : <?php the_field("prix")?> euros.<br>
+                            <a href="<?php the_permalink() ?>"> Details</a>
+                        </p>
+                    </div>
+                </div>
+            
+            </div>
+            </div>
+
              
-                <?php the_post_thumbnail('medium') ?>
+            
                
-                <h2 ><?php the_field("nom") ?></h2>
-                        <h3>
-                           Prix: <?php the_field("prix") ?>
-                        </h3>
-                        <a href="<?php the_permalink() ?>"> Details</a>
+              
              
 <?php endwhile; wp_reset_query(); ?>
 
--- app/public/wp-content/themes/azhari_abderrahman_theme/single-logement.php
+++ app/public/wp-content/themes/azhari_abderrahman_theme/single-logement.php
@@ -2,29 +2,31 @@
 
 <?php if (have_posts()): ?>
         <?php while(have_posts()): the_post(); ?>
-            <h1><?php the_field("nom") ?></h1>
-            <?php the_post_thumbnail('medium') ?>
-            <p>
-            	<?php the_field("description")?>
-            </p>
-            <h3><?php the_field("type")?></h3>
-            <p>
-            <?php the_field("surface")?>
-            </p>
-            <p> frais d'agence :
-            <?php the_field("frais_agence")?> euros
-            </p>
-            <p>
-            <?php the_field("prix")?> euros
-            </p>
-            <p>
-                mise en vente en : 
-            <?php the_field("date_mise_vente")?>
-            </p>
-            <p>
-                exposition : 
-            <?php the_field("exposition")?>
-            </p>
+
+            <div class="center">
+                <div class="property-card">
+                    <a href="#">
+                        
+                        <div class="property-image">
+                            
+                                <?php the_post_thumbnail('medium') ?>
+                            
+                        </div>
+                    </a>
+                    <div class="property-description">
+                        <h5> <?php the_field("nom") ?> </h5>
+                        <p>Un bien de type : <?php the_field("type")?>.<br>
+                            Surface : <?php the_field("surface")?>
+                            m².<br>
+                            Exposition : <?php the_field("exposition")?><br>
+                            Frais d'agence : <?php the_field("frais_agence")?> euros.<br>
+                            Prix : <?php the_field("prix")?> euros.<br>
+                            Mise en vente en : <?php the_field("date")?>.<br>
+                        </p>
+                    </div>
+                </div>
+            </div>
+
             
 
             <?php the_content() ?>
